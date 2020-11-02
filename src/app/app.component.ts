f7a8124345c8a0fa82e74153a61a2d83fc8be978
archigoyal <archigoyal1998@gmail.com>
2020-11-02 12:57:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs' ;
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
+
+interface Task{
+  uuid: string;
+  title: string;
+  description: string;
+  user: User;
+}
+
+interface  User{
+  uuid: string;
+  displayNAme: string;
+}
+
+interface Response {
+  tasks: Task[];
+}
+
+const GET_TASKS= gql `
+query MyQuery {
+  tasks {
+    uuid
+    title
+    description
+    user {
+      fullName
+      uuid
+    }
+  }
+}
+
+
+`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tut';
+
+  tasks$: Observable<Task[]>;
+
+  constructor(private apollo: Apollo){
+
+  }
+  ngOnInit(): void {
+    this.tasks$=this.apollo
+    .watchQuery<Response>({
+      query: GET_TASKS,
+    })
+    .valueChanges.pipe(map((result) => result.data.tasks));
+  }
+
 }
